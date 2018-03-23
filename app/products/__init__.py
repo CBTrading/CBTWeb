87a8d7c2bff3b70a45f32148e67e59470889d025
Alfredo Mejia-Narvaez <mejia@cida.gob.ve>
2018-03-22 23:02:49 -0400

    updating default data structure

--- app/products/__init__.py
+++ app/products/__init__.py
@@ -4,11 +4,10 @@ client = account.Client()
 
 instruments = account.Instruments(client).table
 
-default_instruments = {"candles": "WTICO_USD", "charts": ["EUR_USD", "USD_JPY", "GBP_USD", "XAU_USD"]}
-default_resolutions = {"candles": "H1", "charts": "H1"}
-default_datetimes = {"candles": ("2018-02-01", "2018-02-10"), "charts": ("2018-02-01", "2018-02-10")}
-default_timezone = "America/New_York"
-default_names = {
-    "candles": instruments["name"][instruments["symbol"].index(default_instruments["candles"])],
-    "charts": [instruments["name"][instruments["symbol"].index(symbol)] for symbol in default_instruments["charts"]]
+candles_def = {
+    "symbol": "WTICO_USD",
+    "name": instruments["name"][instruments["symbol"].index("WTICO_USD")],
+    "resolution": "H1",
+    "datetimes": ("2018-02-01", "2018-02-10"),
+    "timezone": "America/New_York"
 }
