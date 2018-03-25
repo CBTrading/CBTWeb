de384ab2194e350ac30d9f7e1a86539fafb005ea
Alfredo Mejia-Narvaez <mejia@cida.gob.ve>
2018-03-25 15:26:29 -0400

    updating charts defaults & handling

--- app/products/__init__.py
+++ app/products/__init__.py
@@ -4,10 +4,19 @@ client = account.Client()
 
 instruments = account.Instruments(client).table
 
+resolution_def = "H1"
+datetimes_def = ("2018-02-01", "2018-02-10")
+timezone_def = "America/New_York"
+
+candles_symbol = "WTICO_USD"
+charts_symbols = ["EUR_USD", "USD_JPY", "GBP_USD", "XAU_USD"]
+charts_time_groups = {"H1": [4, 6, 8, 12], "D": [5, 10, 14, 22]}
+
 candles_def = {
-    "symbol": "WTICO_USD",
-    "name": instruments["name"][instruments["symbol"].index("WTICO_USD")],
-    "resolution": "H1",
-    "datetimes": ("2018-02-01", "2018-02-10"),
-    "timezone": "America/New_York"
+    "symbol": candles_symbol,
+    "name": instruments["name"][instruments["symbol"].index(candles_symbol)],
+    "resolution": resolution_def,
+    "datetimes": datetimes_def,
+    "timezone": timezone_def
+}
 }
