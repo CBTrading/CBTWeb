52f94923cdf2266b8812c4b54cc062cf3c4f5955
Alfredo Mejia-Narvaez <mejia@cida.gob.ve>
2018-03-28 23:37:04 -0400

    removing datetime widget

--- app/products/forms.py
+++ app/products/forms.py
@@ -5,46 +5,9 @@ from wtforms.widgets import HTMLString, html_params
 
 from . import instruments
 
-class DatePickerWidget(object):
-    """
-    Date Time picker from Eonasdan GitHub
-
-    https://github.com/dpgaspar/Flask-AppBuilder/blob/master/flask_appbuilder/fieldwidgets.py#L5
-    """
-    data_template = ('<div id="datetimepicker" class="input-append">'
-                     '<input data-format="yyyy-MM-dd" type="text"></input>'
-                     '<span class="add-on">'
-                     '<i data-time-icon="icon-time" data-date-icon="icon-calendar"></i>'
-                     '</span>'
-                     '</div>')
-                    # '<div class="input-group date appbuilder_date" id="datepicker">'
-                    # '<span class="input-group-addon"><i class="fa fa-calendar cursor-hand"></i>'
-                    # '</span>'
-                    # '<input data-format="yyyy-MM-dd" %(text)s/>'
-                    # '</div>'
-
-    def __call__(self, field, **kwargs):
-        kwargs.setdefault('id', field.id)
-        kwargs.setdefault('name', field.name)
-        if not field.data:
-            field.data = ""
-        template = self.data_template
-
-        return HTMLString(template % {'text': html_params(type='text',
-                                      value=field.data,
-                                      **kwargs)
-                                      })
 
 class CandlesForm(FlaskForm):
     instrument = SelectField("Instrument", choices=zip(instruments["symbol"], instruments["name"]))
     resolution = SelectField("Resolution", choices=zip("M15 M30 H1 H6 H12 D".split(), "M15 M30 H1 H6 H12 D".split()))
-    start_datetime = DateField(
-        "Start at",
-        [required()],
-        widget=DatePickerWidget()
-    )
-    end_datetime = DateField(
-        "End at",
-        [required()],
-        widget=DatePickerWidget()
-    )
+    start_datetime = DateField("Start at", validators=[required()])
+    end_datetime = DateField("End at", validators=[required()])
