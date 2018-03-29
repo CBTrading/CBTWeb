40d85af648aaf3b4e5e12db0b989891398d9b82b
Alfredo Mejia-Narvaez <mejia@cida.gob.ve>
2018-03-29 16:28:38 -0400

    implementing custom field & widget for date picking

--- app/products/forms.py
+++ app/products/forms.py
@@ -6,8 +6,42 @@ from wtforms.widgets import HTMLString, html_params
 from . import instruments
 
 
+class DateRangeField(DateField):
+
+    def _value(self):
+        print self.data
+        if self.data:
+            return u" ".join(self.data)
+        else:
+            return u""
+
+    def process_formdata(self, date_range):
+        if date_range:
+            self.data = date_range
+        else:
+            self.data = []
+
+class DateRangePickerWidget(object):
+    data_template = (
+        '<div class="input-group input-daterange col-md-12">'
+        '<input %(input-1)s>'
+        '<div class="input-group-addon">to</div>'
+        '<input %(input-2)s>'
+        '</div>'
+    )
+
+    def __call__(self, field, **kwargs):
+        if not field.data:
+            data_1, data_2 = "", ""
+        else:
+            data_1, data_2 = field.data
+
+        return HTMLString(self.data_template % {
+            "input-1": html_params(id="{}-1".format(field.id), name="{}".format(field.name), type="text", value=data_1, **kwargs),
+            "input-2": html_params(id="{}-2".format(field.id), name="{}".format(field.name), type="text", value=data_2, **kwargs)
+        })
+
 class CandlesForm(FlaskForm):
     instrument = SelectField("Instrument", choices=zip(instruments["symbol"], instruments["name"]))
     resolution = SelectField("Resolution", choices=zip("M15 M30 H1 H6 H12 D".split(), "M15 M30 H1 H6 H12 D".split()))
-    start_datetime = DateField("Start at", validators=[required()])
-    end_datetime = DateField("End at", validators=[required()])
+    datetime_range = DateRangeField("Date range", validators=[required()], widget=DateRangePickerWidget())
